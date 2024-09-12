7a559215093f857b0f248549d574c14b51e67854
Leanid <leanid.sushchynski@gmail.com>
2024-09-12 18:55:49 +0300

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+
+
+export const routes: Routes = [
+   
+        {path:'',component:ProfileCardComponent},
+       
+    ]
+    
+    
+   
+
